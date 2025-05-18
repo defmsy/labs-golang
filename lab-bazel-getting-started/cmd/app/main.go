8425d9f4913455f3edaa474678d5fba958ed8515
Jean-Luc Mounsamy <jean-luc.mounsamy@msy-tech.net>
2025-05-18 19:56:09 -0400

    chore(lab-bazel-getting-started): configure gin-gonic

--- lab-bazel-getting-started/cmd/app/main.go
+++ lab-bazel-getting-started/cmd/app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, Bazel!")
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run(":8090")
 }
